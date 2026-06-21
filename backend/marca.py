8d77744b27d8f78000b1d54ece963bf16f86a69f
caetanosiljr <caetanosil.jr@gmail.com>
2026-06-21 18:19:34 -0300

    Update marca.py

--- backend/marca.py
+++ backend/marca.py
@@ -1,5 +1,6 @@
 import conexaoBD
 
+
 def Listar_Marca():
     mydb = conexaoBD.conectar()
     mycursor = mydb.cursor()
@@ -8,10 +9,6 @@ def Listar_Marca():
     mydb.close()
     return myresult
 
-def Printar_Marca():
-    marcas = Listar_Marca()
-    for n in range(len(marcas)):
-        print(f"{n+1}- {marcas[n]}")
 
 def Atributos_Marca():
     mydb = conexaoBD.conectar()
@@ -19,89 +16,48 @@ def Atributos_Marca():
     mycursor.execute("SHOW COLUMNS FROM Marca")
     atributos = mycursor.fetchall()
     mydb.close()
+
     colunas = []
     for n in range(len(atributos)):
         colunas.append(atributos[n][0])
     return colunas
 
-def Printar_Atributos():
-    marcas = Atributos_Marca()
-    for n in range(len(marcas)):
-        print(f"{n+1}- {marcas[n]}")
 
-def Cadastrar_Marca(valores): 
+def Cadastrar_Marca(valores):
     try:
         mydb = conexaoBD.conectar()
         mycursor = mydb.cursor()
         colunas_lista = Atributos_Marca()
         colunas_string = ",".join(colunas_lista)
-        
         placeholders = ",".join(["%s"] * len(colunas_lista))
 
         sqlInsert = f"INSERT INTO Marca ({colunas_string}) VALUES ({placeholders})"
-
-        mycursor.execute(sqlInsert, valores)
+        mycursor.execute(sqlInsert, tuple(valores))
         mydb.close()
-        
         return True
     except:
         return False
 
-def Update_Marca(marcaID, coluna, novo_valor):
+
+def Update_Marca(cnpj, coluna, novo_valor):
     try:
         mydb = conexaoBD.conectar()
         mycursor = mydb.cursor()
-        cnpj = marcaID
         sqlUpdate = f"UPDATE Marca SET {coluna} = %s WHERE CNPJ = %s"
         mycursor.execute(sqlUpdate, (novo_valor, cnpj))
         mydb.close()
-        
         return True
     except:
         return False
 
-def Delet_Marca(marcaID):
+
+def Delet_Marca(cnpj):
     try:
         mydb = conexaoBD.conectar()
         mycursor = mydb.cursor()
-
-        cnpj = marcaID
-        sqlDelete = f"DELETE FROM Marca WHERE CNPJ = '{cnpj}'"
-        mycursor.execute(sqlDelete)
+        sqlDelete = "DELETE FROM Marca WHERE CNPJ = %s"
+        mycursor.execute(sqlDelete, (cnpj,))
         mydb.close()
-        
         return True
     except:
         return False
-
-def Marca(op):
-    if op == 1:
-        print("\n--- CADASTRAR MARCA ---")
-        atributos = Atributos_Marca()
-        valores = []
-        for atributo in atributos:
-            valor = input(f"{atributo}: ")
-            valores.append(valor)
-        Cadastrar_Marca(tuple(valores))
-        print("Marca cadastrada com sucesso!")
-    
-    elif op == 2:
-        print("\n--- ALTERAR MARCA ---")
-        Printar_Marca()
-        posicao = int(input("Escolha a marca: "))
-        Printar_Atributos()
-        atributo = int(input("Escolha o atributo: "))
-        novo_valor = input("Novo valor: ")
-        Update_Marca(posicao, atributo, novo_valor)
-        print("Marca alterada com sucesso!")
-    
-    elif op == 3:
-        print("\n--- DELETAR MARCA ---")
-        Printar_Marca()
-        posicao = int(input("Escolha a marca: "))
-        Delet_Marca(posicao)
-        print("Marca deletada com sucesso!")
-    
-    elif op == 4:
-        print("\n--- LISTAR MARCAS ---")
-        Printar_Marca()
